19fd8d890958084606b4a3d9149b98606a9a425c
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-06-27 12:09:33 +0200

    Subir imágenes de los proyectos

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Project} from '../../models/project';
 import { ProjectService } from "../../services/project.service";
+import { UploadService } from '../../services/upload.service';
+import { Global } from '../../services/global';
 
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
 
   public title: string;
   public project: Project;
   public status: string;
+  public filesToUpload: Array<File>;
 
   constructor(
-    private _projectService: ProjectService
+    private _projectService: ProjectService,
+    private _uploadService: UploadService
+
   ) { 
     this.title = 'Crear proyecto';
     this.project = new Project('','','','',2018,'','');
@@ -29,9 +34,14 @@ export class CreateComponent implements OnInit {
     this._projectService.saveProject(this.project).subscribe(
       response => {
         if (response.project) {
-          this.status = 'success';
-          window.scrollTo(0, 0);
-          form.reset();
+          //Subir imagen
+          this._uploadService.makeFileRequest(Global.url + 'upload-image/' + response.project._id, [], this.filesToUpload, 'image' ).then((result:any) => {
+            console.log(result); 
+            this.status = 'success';
+            window.scrollTo(0, 0);
+            form.reset();
+          });
+
         } else{
           this.status = 'failed';
         }
@@ -42,5 +52,9 @@ export class CreateComponent implements OnInit {
         
       }
     )
+  }
+
+  fileChangeEvent(fileInput:any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 }
